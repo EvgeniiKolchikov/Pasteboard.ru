f0f40b90d35c473ae5f14068571c1f0d75f2b060
= <evgeniy_kolchikov@mail.ru>
2022-12-07 19:47:54 +0300

    Представление для добавление карточки, получение данных с представления и запись в репозиторий

--- PasteboardProject/PasteboardProject/Controllers/PasteboardController.cs
+++ PasteboardProject/PasteboardProject/Controllers/PasteboardController.cs
@@ -8,35 +8,41 @@ namespace PasteboardProject.Controllers;
 
 public class PasteboardController : Controller
 {
-    private PasteboardRepository pasteboardRepository;
+    private PasteboardRepositoryJson pasteboardRepository;
     public PasteboardController()
     {
-        pasteboardRepository = new PasteboardRepository();
+        pasteboardRepository = new PasteboardRepositoryJson();
     }
     [HttpGet]
     public IActionResult ShowPasteboard(int id)
     {
-        var pasteboardById = pasteboardRepository.GetPasteboardById(id); 
+        var pasteboardById = pasteboardRepository.GetPasteboardById(id);
         return View(pasteboardById);
     }
     
     [HttpGet]
     public IActionResult CreatePasteboard()
     {
-        var pasteboard = new Pasteboard()
-        {
-            PasteboardFields = new List<PasteboardField>()
-        };
-        pasteboardRepository.CreatePasteboard(pasteboard);
-        var count = pasteboardRepository.Pasteboards.Count;
         return View();
     }
+
     [HttpPost]
-    public IActionResult CreatePasteboard(Pasteboard pasteboard)
+    // public IActionResult CreatePasteboard(Pasteboard pasteboard)
+    // {
+    //     var lastId = pasteboardRepository.Pasteboards.Count;
+    //     pasteboard.Id = lastId + 1;
+    //     pasteboardRepository.CreatePasteboard(pasteboard);
+    //     var count = pasteboardRepository.Pasteboards.Count;
+    //     Log.Debug("CardController created");
+    //     return RedirectToAction("ShowPasteboard", pasteboard);
+    // }
+    public async Task<IActionResult> CreatePasteboard(Pasteboard pasteboard)
     {
-        pasteboardRepository.CreatePasteboard(pasteboard);
-        var count = pasteboardRepository.Pasteboards.Count;
-        Log.Debug("CardController created");
+        var lastId = pasteboardRepository.Pasteboards.Count;
+        pasteboard.Id = lastId + 1;
+        pasteboardRepository.Pasteboards.Add(pasteboard);
+        await pasteboardRepository.AddCardToJsonAsync();
+        object id = pasteboard.Id;
         return View();
     }
 }
--- PasteboardProject/PasteboardProject/Models/Pasteboard.cs
+++ PasteboardProject/PasteboardProject/Models/Pasteboard.cs
@@ -5,4 +5,9 @@ public class Pasteboard
     public int Id { get; set; }
     public string Name { get; set; }
     public List<PasteboardField> PasteboardFields { get; set; }
+
+    public Pasteboard()
+    {
+        PasteboardFields = new List<PasteboardField>();
+    }
 }
