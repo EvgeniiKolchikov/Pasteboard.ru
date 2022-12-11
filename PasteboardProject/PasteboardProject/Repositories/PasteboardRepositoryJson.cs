732de23bf6df56302031608627ad9a68f75c12b6
= <evgeniy_kolchikov@mail.ru>
2022-12-11 22:47:01 +0300

    Изменил метод Add

--- PasteboardProject/PasteboardProject/Repositories/PasteboardRepositoryJson.cs
+++ PasteboardProject/PasteboardProject/Repositories/PasteboardRepositoryJson.cs
@@ -15,11 +15,27 @@ public class PasteboardRepositoryJson
 
     public Pasteboard GetPasteboardById(int pasteboardId)
     {
-        return Pasteboards.First(pb => pb.Id == pasteboardId);
+        var pasteboard = new Pasteboard();
+        try
+        {
+            pasteboard = Pasteboards[pasteboardId - 1];
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            var count = Pasteboards.Count;
+            var rnd = new Random();
+            var rndIndex = rnd.Next(1, count + 1);
+            pasteboard = Pasteboards[rndIndex];
+        }
+
+        return pasteboard;
     }
 
-    public async Task AddCardToJsonAsync()
+    public async Task AddCardToJsonAsync(Pasteboard pasteboard)
     {
+        var lastId = Pasteboards.Count;
+        pasteboard.Id = lastId + 1;
+        Pasteboards.Add(pasteboard);
         using (var fs = new FileStream("pasteboard.json",FileMode.OpenOrCreate))
         {
             await JsonSerializer.SerializeAsync(fs, Pasteboards);
