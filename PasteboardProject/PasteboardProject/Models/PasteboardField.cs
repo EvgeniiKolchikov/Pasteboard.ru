bc27cf4f6e9efbf27baf669fe5b57b3090e5da32
= <evgeniy_kolchikov@mail.ru>
2022-12-14 20:24:36 +0300

    Изменил FieldName FieldValue на nullable тип

--- PasteboardProject/PasteboardProject/Models/PasteboardField.cs
+++ PasteboardProject/PasteboardProject/Models/PasteboardField.cs
@@ -3,7 +3,7 @@ namespace PasteboardProject.Models;
 public class PasteboardField
 {
     public int Id { get; set; }
-    public string FieldName { get; set; }
-    public string FieldValue { get; set; }
+    public string? FieldName { get; set; }
+    public string? FieldValue { get; set; }
     public int PasteboardId { get; set; }
 }
