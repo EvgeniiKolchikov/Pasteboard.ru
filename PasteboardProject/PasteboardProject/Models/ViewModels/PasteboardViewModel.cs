0593d858641544b2d060845f628ffd343f6c2cb9
= <evgeniy_kolchikov@mail.ru>
2022-12-19 22:22:57 +0300

    добавлено свойство AspAction для передачи в View

--- PasteboardProject/PasteboardProject/Models/ViewModels/PasteboardViewModel.cs
+++ PasteboardProject/PasteboardProject/Models/ViewModels/PasteboardViewModel.cs
@@ -3,9 +3,6 @@ namespace PasteboardProject.Models.ViewModels;
 public class PasteboardViewModel
 {
     public Pasteboard Pasteboard { get; set; }
+    public string AspAction { get; set; }
     
-    public PasteboardViewModel()
-    {
-        Pasteboard = new Pasteboard();
-    }
 }
