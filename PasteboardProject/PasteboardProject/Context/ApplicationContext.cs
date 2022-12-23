aee0a086d3c83fadf93c506b09e7346b8c068007
= <evgeniy_kolchikov@mail.ru>
2022-12-23 20:43:52 +0300

    Класс контекст

---
+++ PasteboardProject/PasteboardProject/Context/ApplicationContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using PasteboardProject.Models;
+
+namespace PasteboardProject.Context;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Pasteboard> Pasteboards { get; set; }
+    public DbSet<PasteboardField> PasteboardFields { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+}
