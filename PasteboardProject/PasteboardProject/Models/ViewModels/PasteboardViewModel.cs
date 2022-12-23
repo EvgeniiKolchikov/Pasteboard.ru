4be2e461bb6903b98739aaaf50a7b8fc52553cdf
= <evgeniy_kolchikov@mail.ru>
2022-12-23 20:40:41 +0300

    Добавил List<ActivePasteboardField> во viewmodel

--- PasteboardProject/PasteboardProject/Models/ViewModels/PasteboardViewModel.cs
+++ PasteboardProject/PasteboardProject/Models/ViewModels/PasteboardViewModel.cs
@@ -4,5 +4,5 @@ public class PasteboardViewModel
 {
     public Pasteboard Pasteboard { get; set; }
     public string AspAction { get; set; }
-    
+    public List<ActivePasteboardField> ActivePasteboardFields { get; set; }
 }
