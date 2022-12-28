abe821e7e39f5a2cc62665412341fdad6fad5b1e
= <evgeniy_kolchikov@mail.ru>
2022-12-28 22:30:01 +0300

    работа над методами записи и получения данных из репозитория

--- PasteboardProject/PasteboardProject/Repositories/PasteboardRepositorySql.cs
+++ PasteboardProject/PasteboardProject/Repositories/PasteboardRepositorySql.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using PasteboardProject.Context;
 using PasteboardProject.Interfaces;
 using PasteboardProject.Models;
 
@@ -5,13 +7,42 @@ namespace PasteboardProject.Repositories;
 
 public class PasteboardRepositorySql : IRepository
 {
+    private ApplicationContext _db;
+
+    public PasteboardRepositorySql(ApplicationContext context)
+    {
+        _db = context;
+    }
+    
     public Pasteboard GetPasteboardById(int id)
     {
-        throw new NotImplementedException();
+         var pasteboard = _db.Pasteboards.FirstAsync(p => p.Id == id).Result;
+         var pasteboardField = _db.PasteboardFields.Where(pf => pf.PasteboardId == id).ToList();
+         pasteboard.PasteboardFields = pasteboardField;
+         return pasteboard;
     }
 
-    public Task AddPasteboardAsync(Pasteboard pasteboard)
+    public async Task AddPasteboardAsync(Pasteboard pasteboard)
     {
-        throw new NotImplementedException();
+        var pasteboardExist = await _db.Pasteboards.AnyAsync(p => p.Id == pasteboard.Id);
+        //firstofdefault()
+        if (!pasteboardExist) //id == 0
+        {
+            _db.Pasteboards.Add(pasteboard);
+            await _db.SaveChangesAsync();
+        }
+        else
+        {
+            var oldFields = _db.PasteboardFields.Where(pf => pf.PasteboardId == pasteboard.Id);
+            foreach (var pf in oldFields)
+            {
+                pf.PasteboardId = null;
+            }
+            _db.PasteboardFields.UpdateRange(oldFields);
+            await _db.SaveChangesAsync();
+            _db.Pasteboards.Update(pasteboard);
+            _db.PasteboardFields.AddRange(pasteboard.PasteboardFields);
+            await _db.SaveChangesAsync();
+        }
     }
 }
