72155a150e8904b3ed9893df32133fb8633ffac8
= <evgeniy_kolchikov@mail.ru>
2023-01-08 20:41:03 +0300

    убрал nullable

--- PasteboardProject/PasteboardProject/Models/PasteboardField.cs
+++ PasteboardProject/PasteboardProject/Models/PasteboardField.cs
@@ -5,7 +5,7 @@ namespace PasteboardProject.Models;
 public class PasteboardField
 {
     public int Id { get; set; }
-    [Required] public string? FieldName { get; set; } = "";
-    [Required] public string? FieldValue { get; set; } = "";
+    [Required] public string FieldName { get; set; } = "";
+    [Required] public string FieldValue { get; set; } = "";
     public int PasteboardId { get; set; }
 }
