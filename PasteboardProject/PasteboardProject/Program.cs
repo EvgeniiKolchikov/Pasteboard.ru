21a2f4d046f982cc90562b9590c1b1948fe5df82
= <evgeniy_kolchikov@mail.ru>
2023-01-11 21:37:21 +0300

    подготовка к запуску

--- PasteboardProject/PasteboardProject/Program.cs
+++ PasteboardProject/PasteboardProject/Program.cs
@@ -14,12 +14,12 @@ var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
+    app.UseHttpsRedirection();
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
