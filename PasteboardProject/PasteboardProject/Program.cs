3bcd55f9582407b7b1a35921aabd029bf3a1f80a
= <evgeniy_kolchikov@mail.ru>
2023-02-09 19:45:39 +0300

    NLog added

--- PasteboardProject/PasteboardProject/Program.cs
+++ PasteboardProject/PasteboardProject/Program.cs
@@ -1,38 +1,56 @@
-using System.Diagnostics;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
-using NLog.Fluent;
+using NLog;
+using NLog.Web;
 using PasteboardProject.Context;
 using PasteboardProject.Interfaces;
 using PasteboardProject.Middlewares;
 using PasteboardProject.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
-var connection = builder.Configuration.GetConnectionString("PostgresConnection");
-builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection).UseLowerCaseNamingConvention());
-builder.Services.AddTransient<IRepository,PasteboardRepositoryPostgres>();
-builder.Services.AddControllersWithViews();
+var logger = LogManager.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
+logger.Debug("init main");
 
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+    var connection = builder.Configuration.GetConnectionString("PostgresConnection");
+    builder.Services.AddDbContext<ApplicationContext>(options =>
+        options.UseNpgsql(connection).UseLowerCaseNamingConvention());
+    builder.Services.AddTransient<IRepository, PasteboardRepositoryPostgres>();
+    builder.Services.AddControllersWithViews();
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+    
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.UseMiddleware<ResponseTimeMiddleware>();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    logger.Error(e, "Stopped exception");
 }
 
-app.UseHttpsRedirection();
+finally
+{
+    logger.Debug("main stopped");
+    LogManager.Shutdown();
+}
 
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.UseMiddleware<ResponseTimeMiddleware>();
-
-app.MapControllers();
-
-app.Run();
